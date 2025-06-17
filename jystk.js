1e63ba711945d01a11faa6f3895d56b4a2d03062
CRW-MATE <94070425+CRW-MATE@users.noreply.github.com>
2025-06-17 23:52:19 +0300

    Add files via upload

--- jystk.js
+++ jystk.js
@@ -1,39 +1,66 @@
+let innerH = window.innerHeight;
+let innerW = window.innerWidth;
+let mh;
+let mx;
+let innerD;
 let log;
 let int;
-let ds = 45;
+let ds;
+let dire = "N";
 const joy = document.getElementById("joy");
 const hub = document.querySelector("#joyHub").style;
 //hubStyle
-let hubx = 35,
-  huby = 10,
-  hubs = 300;
-hub.opacity = "40%";
-hub.border = "double 15px white";
-hub.backgroundColor = "gray";
-hub.height = `${hubs}px`;
-hub.width = `${hubs}px`;
-hub.translate = `${hubx}px ${huby}px`;
 hub.position = "absolute";
+hub.backgroundImage = "url('ui/joyhub.png')";
+hub.backgroundSize = "cover";
 hub.zIndex = "3";
-//joyStyles
-let joys = 90;
-joy.style.height = `${joys}px`;
-joy.style.width = `${joys}px`;
-joy.style.backgroundColor = "rgb(113, 37,37)";
+let Alp = ["A", "B", "C", "D"];
+//hubs/3tyles
 joy.style.textAlign = "center";
-joy.style.color = "azure";
-joy.style.translate = `${hubs / 3}px ${hubs / 3}px`;
-//joystick apperance "nub border amd main loop for input"
-const joyST = (e, xrc) => {
-  log = [
-    Math.floor(e.touches[0].clientX) - joys - hubx,
-    Math.floor(e.touches[0].clientY) - joys - huby,
-  ];
-  if (log[0] > hubs - joys) {
-    log[0] = hubs - joys;
+joy.style.backgroundImage = "url('ui/fing.png')";
+joy.style.backgroundSize = "cover";
+//hubs/3tick apperance "nub border amd main loop for input"
+let hubs, hubx, huby;
+const buttons = document.getElementsByTagName("button");
+const jySize = (a, b, c) => {
+  hub.height = `${a}px`;
+  hub.width = `${a}px`;
+  hubs = a;
+  ds = a / 6;
+  hubx = b;
+  huby = c;
+  joy.style.height = `${(a * 9) / 15}px`;
+  joy.style.width = `${a / 3}px`;
+  hub.translate = `${b}px ${c}px`;
+  joy.style.translate = `${a / 3}px ${a / 3}px`;
+  for (i = 0; i < buttons.length; i++) {
+    let a = buttons[i].style;
+    a.backgroundImage = `url("ui/${Alp[i]}.png")`;
+    a.backgroundSize = "cover";
+    a.backgroundColor = "rgba(0,0,0, 0)";
+    a.margin = "0px";
+    a.padding = "0px";
+    a.border = "0px";
+    a.height = `150px`;
+    a.width = `150px`;
+    a.position = "absolute";
+    a.zIndex = "3";
+    a.textAlign = "center";
+    a.userSelect = "none";
+    a.fontSize = " 100px";
   }
-  if (log[1] > hubs - joys) {
-    log[1] = hubs - joys;
+};
+const joyST = (e) => {
+  e.preventDefault();
+  log = [
+    Math.floor(e.touches[0].clientX) - hubs / 6 - hubx,
+    Math.floor(e.touches[0].clientY) - (9 * hubs) / 30 - huby,
+  ];
+  if (log[0] > hubs - hubs / 3) {
+    log[0] = hubs - hubs / 3;
+  }
+  if (log[1] > hubs - hubs / 3) {
+    log[1] = hubs - hubs / 3;
   }
   if (log[0] < 0) {
     log[0] = 0;
@@ -41,81 +68,104 @@ const joyST = (e, xrc) => {
   if (log[1] < 0) {
     log[1] = 0;
   }
+  if (log[0] < hubs / 3 - hubs / 6) {
+    joy.style.backgroundImage = "url('ui/fingL.png')";
+  } else {
+    joy.style.backgroundImage = "url('ui/fing.png')";
+  }
   joy.style.translate = `${log[0]}px  
 ${log[1]}px`;
-  if (xrc == 2) {
-    int = setInterval(function () {
-      if (log[1] > hubs - joys - ds) {
-        //  Pp1.y = Pp1.y + Pp1.ver;
-        joy.innerHTML = "D";
+  if (log[1] > hubs / 3 + ds) {
+    dire = "D";
+    // joy.innerHTML = "D";
+  }
+  if (log[1] < hubs / 3 - ds) {
+    dire = "U";
+    //joy.innerHTML = "U";
+  }
+  if (log[0] > hubs / 3 + ds) {
+    if (log[1] > hubs / 3 + ds) {
+      dire = "DR";
+      //joy.innerHTML = "DR";
+    } else {
+      if (log[1] < hubs / 3 - ds) {
+        dire = "UR";
+        //joy.innerHTML = "UR";
+      } else {
+        dire = "R";
+        // joy.innerHTML = "R";
       }
-      if (log[1] < joys - ds) {
-        // Pp1.y = Pp1.y - Pp1.ver;
-        joy.innerHTML = "U";
-      }
-      if (log[0] > hubs - joys - ds) {
-        if (log[1] > hubs - joys - ds) {
-          Pp1.x = Pp1.x + Pp1.vel * 0.707;
-          Pp1.y = Pp1.y + Pp1.vel * 0.707;
-          joy.innerHTML = "DR";
-        } else {
-          if (log[1] < joys - ds) {
-            Pp1.x = Pp1.x + Pp1.vel * 0.707;
-            //Pp1.y = Pp1.y - Pp1.vel * 0.707;
-            joy.innerHTML = "UR";
-          } else {
-            Pp1.x = Pp1.x + Pp1.vel;
-            joy.innerHTML = "R";
-          }
-        }
-      }
-      if (joys - ds < log[0] && log[0] < hubs - joys - ds) {
-        if (joys - ds < log[1] && log[1] < hubs - joys - ds) {
-          joy.innerHTML = "N";
-        }
-      }
+    }
+  }
+  if (hubs / 3 - ds < log[0] && log[0] < hubs / 3 + ds) {
+    if (hubs / 3 - ds < log[1] && log[1] < hubs / 3 + ds) {
+      //joy.innerHTML = "N";
+      dire = "N";
+    }
+  }
 
-      if (log[0] < joys - ds) {
-        if (log[1] > hubs - joys - ds) {
-          Pp1.x = Pp1.x - Pp1.vel * 0.707;
-          //Pp1.y = Pp1.y + Pp1.vel * 0.707;
-          joy.innerHTML = "DL";
-        } else {
-          if (log[1] < joys - ds) {
-            Pp1.x = Pp1.x - Pp1.vel * 0.707;
-            //Pp1.y = Pp1.y - Pp1.vel * 0.707;
-            joy.innerHTML = "UL";
-          } else {
-            Pp1.x = Pp1.x - Pp1.vel;
-            joy.innerHTML = "L";
-          }
-        }
+  if (log[0] < hubs / 3 - ds) {
+    if (log[1] > hubs / 3 + ds) {
+      dire = "DL";
+      //joy.innerHTML = "DL";
+    } else {
+      if (log[1] < hubs / 3 - ds) {
+        dire = "UL";
+        //joy.innerHTML = "UL";
+      } else {
+        dire = "L";
+        //joy.innerHTML = "L";
       }
-    }, 100);
+    }
   }
 };
 
 const joyEnd = () => {
-  joy.innerHTML = "N";
+  //joy.innerHTML = "N";
+  dire = "N";
   log = [];
-  vG = 0;
-  clearInterval(int);
+  joy.style.backgroundImage = "url('ui/fing.png')";
   joy.style.translate = `${hubs / 3}px ${hubs / 3}px`;
 };
-//buttons postition and styles
-const buttons = document.getElementsByClassName("butt");
-for (i = 0; i < buttons.length; i++) {
-  let a = buttons[i].style;
-  a.opacity = "40%";
-  a.border = "double 15px white";
-  a.backgroundColor = "gray";
-  a.height = `${150}px`;
-  a.width = `${150}px`;
-  a.translate = `${hubx}px ${huby}px`;
-  a.position = "absolute";
-  a.zIndex = "3";
-}
-buttons[0].style.translate = "575px 10px";
-buttons[1].style.translate = `775px 10px`;
-buttons[2].style.translate = `575px 185px`;
-buttons[3].style.translate = `775px 185px`;
+const button = (a, b) => {
+  if (b) {
+    buttons[a].style.backgroundImage = `url("ui/${Alp[a]}1.png")`;
+  } else {
+    buttons[a].style.backgroundImage = `url("ui/${Alp[a]}.png")`;
+  }
+};
+const screenResize = (X) => {
+  /*  if (X) {
+    innerD = innerH;
+    innerH = innerW;
+    innerW = innerD;
+  }
+  if (innerW >= innerH) {*/
+  mh = 800;
+  mx = 1000;
+  jySize(450, 10, (1 / 3) * mh - 50);
+  buttons[0].style.translate = `${innerW - mx / 2 + 50}px ${250}px`;
+  buttons[1].style.translate = `${innerW - mx / 2 + 300}px ${400}px`;
+  buttons[2].style.translate = `${innerW - mx / 2 + 50}px ${550}px`;
+  buttons[3].style.translate = `${(innerW - mx) / 2 - 200}px ${huby - 200}px`;
+  /* } else {
+    mx = 1000;
+    mh = 800;
+    jySize(500, 10, mh + 100);
+    buttons[3].style.translate = `${hubx + hubs + 100}px ${mh + 100}px`;
+    buttons[1].style.translate = `${innerW - mx / 2 + 300}px ${huby + hubs}px`;
+    buttons[2].style.translate = `${innerW - mx / 2 + 50}px ${
+      huby + hubs + 100
+    }px`;
+    buttons[0].style.translate = `${innerW - mx / 2 + 300}px ${
+      huby + hubs + 250
+    }px`;
+  }*/
+
+  kindow.style.width = `${innerW}px`;
+  kindow.style.height = `${innerH}px`;
+
+  canvas.style.translate = `${(innerW - mx) / 2}px`;
+  canvas.height = mh;
+  canvas.width = mx;
+};
